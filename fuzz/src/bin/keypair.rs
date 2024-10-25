dd2a5eecb6164bd2892c0176e04e6a87349ee5ff
root <sontapaa.jokulainen@gmail.com>
2024-10-25 21:16:59 +0300

    Now builds fuzzers correctly

--- fuzz/src/bin/keypair.rs
+++ fuzz/src/bin/keypair.rs
@@ -1,8 +1,8 @@
 fn main() {
     #[cfg(feature = "fuzz")]
     afl::fuzz!(|data: &[u8]| {
-        use nimiq_bls::{KeyPair};
         use nimiq_serde::Deserialize as _;
+        use nimiq_bls::{KeyPair};
         let _ = KeyPair::deserialize_from_vec(data);
     })
 }
--- fuzz/src/bin/normal_keypair.rs
+++ fuzz/src/bin/normal_keypair.rs
@@ -1,9 +1,8 @@
 fn main() {
     #[cfg(feature = "fuzz")]
     afl::fuzz!(|data: &[u8]| {
-        use nimiq_keys::KeyPair;
         use nimiq_serde::Deserialize as _;
-        use nimiq_primitives::coin::Coin;
+        use nimiq_keys::KeyPair;
         let _ = KeyPair::deserialize_from_vec(data);
     })
 }

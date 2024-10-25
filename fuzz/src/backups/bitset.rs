c94bf25e9835cc30570471b0308dd167adcebedd
root <sontapaa.jokulainen@gmail.com>
2024-10-25 20:16:51 +0300

    Removed backup directory

--- fuzz/src/backups/bitset.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    #[cfg(feature = "fuzz")]
-    afl::fuzz!(|data: &[u8]| {
-        use nimiq_collections::BitSet;
-        use nimiq_serde::Deserialize as _;
-
-        let _ = BitSet::deserialize_from_vec(data);
-    })
-}
--- fuzz/src/backups/trie_node.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    #[cfg(feature = "fuzz")]
-    afl::fuzz!(|data: &[u8]| {
-        use nimiq_primitives::trie::trie_node::TrieNode;
-        use nimiq_serde::Deserialize as _;
-
-        let _ = TrieNode::deserialize_from_vec(data);
-    })
-}

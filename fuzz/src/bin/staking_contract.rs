3f13e9e0d49d5bd32953f243bb78f1b028ab2704
root <sontapaa.jokulainen@gmail.com>
2024-11-04 03:39:58 +0200

    Cleaned up code

--- fuzz/src/bin/staking_contract.rs
+++ fuzz/src/bin/staking_contract.rs
@@ -4,56 +4,18 @@ fn main() {
         use nimiq_serde::{Deserialize, Serialize};
         use nimiq_account::StakingContract;
         let res = StakingContract::deserialize_from_vec(data); // err I think is of type DeserializeError
-
-        // Now check if contr exists. If it does (aka. the original data was a valid staking contract) then try to serialize it back to a vector, then check if the original vector and the new vector are the same, if they aren't then there is a bug in the parsing logic.
-
-        // The existance of error implies that contr does not exist.
-
+        // Now check if contract exists. If it does (aka. the original data was a valid staking contract) then try to serialize it back to a vector, then check if the original vector and the new vector are the same, if they aren't then there is a bug in the parsing logic.
+        // The existence of error implies that contract does not exist.
         match res {
             Ok(v) => {
-                // First calculate the minimum of the two.
-                let otherdata = StakingContract::serialize_to_vec(&v);
-
-                /*
-                let buf = (0..64).collect::<Vec<u8>>();
-                let z: &[u8; 64] = &(&buf[..]).try_into().unwrap();
-                */
-
-                assert!((otherdata.len() <= data.len()), "The size of the serialized version was bigger than the original vector! This shouldn't happen!");
-                //data.resize((otherdata.len()), 0);
-                
-                // let comparison_bullshit = data.collect::<Vec<u8>>(); // Collect the stuff
-
-                // arg[..30].try_into().unwrap()
-
-                let bullshit: &[u8] = data[..(otherdata.len())].try_into().unwrap(); // Yuck!!!
-
-                assert_eq!(bullshit, otherdata);
+                let serialized = StakingContract::serialize_to_vec(&v);
+                assert!((serialized.len() <= data.len()), "The size of the serialized version was bigger than the original vector! This shouldn't happen!");
+                let original_data_segment: &[u8] = data[..(serialized.len())].try_into().unwrap(); // This ugly stuff has to be done, because the serialization function ignores extra bytes at the end so we can not compare the byte vectors by themselves. Yuck!!!
+                assert_eq!(original_data_segment, serialized);
             },
             Err(e) => {
-                // println!("Error thing!!!\n");
                 return;
             },
         }
-
-        /*
-        if (err == None) {
-            // Contr exists
-            assert!(contr, "contr didn't exist, even though err == None!!!!"); // Debug.
-            // Now try to serialize to vec
-            let serialized = StakingContract::serialize_to_vec(contr);
-            // Now check if they are equal:
-            assert_eq!(data, serialized);
-        }
-        */
-
-
     })
 }
-
-/*
-    let contract_2a =
-        StakingContract::deserialize_from_vec(&contract_2.serialize_to_vec()).unwrap();
-
-    assert_eq!(contract_2, contract_2a);
-*/
